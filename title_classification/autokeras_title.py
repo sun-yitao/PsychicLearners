d3429bc07feb47ec5e0ab5d56ca9cb5c0ec86b78
Sun Yitao <sunyitao8@gmail.com>
2019-02-28 00:50:22 +0800

    Added autokeras_title.py

---
+++ title_classification/autokeras_title.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+import autokeras
+from autokeras.text import text_preprocessor, text_supervised
+
+data_directory = os.path.join('..', 'data')
+train = pd.read_csv(os.path.join(data_directory, 'train_split.csv'))
+valid = pd.read_csv(os.path.join(data_directory, 'valid_split.csv'))
+train_x, train_y = train['title'], train['Category']
+valid_x, valid_y = valid['title'], valid['Category']
+
+clf = text_supervised.TextClassifier(verbose=True, path=os.getcwd())
+clf.fit(x=train_x, y=train_y, time_limit=10 * 60 * 60)
+print("Classification accuracy is : ", 100 * clf.evaluate(valid_x, valid_y), "%")
