f86dfc1656cf65e8abb5c0307ae97d6d1e2c49f8
Yitao <sunyitao8@gmail.com>
2019-03-05 16:06:30 +0800

    multi gpu mobile

--- image_classification/mobile_keras.py
+++ image_classification/mobile_keras.py
@@ -11,19 +11,39 @@ from random_eraser import get_random_eraser
 from se_resnext import SEResNextImageNet
 from keras_preprocessing.image import ImageDataGenerator
 from keras import backend as K
+from keras.utils import multi_gpu_model
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,2"
 
 psychic_learners_dir = os.path.split(os.getcwd())[0]
 TRAIN_DIR = os.path.join(psychic_learners_dir, 'data',
                          'image', 'v1_train_nodups_240x240', 'mobile')
 VAL_DIR = os.path.join(psychic_learners_dir, 'data', 'image', 'valid_240x240', 'mobile')
 CHECKPOINT_PATH = os.path.join(psychic_learners_dir, 'data', 'keras_checkpoints', 'mobile')
-EPOCHS = 100  # only for calculation of decay
+EPOCHS = 200  # only for calculation of decay
 IMAGE_SIZE = (240, 240)  # height, width
 N_CLASSES = 27
 MODEL_NO = 1
-LR_BASE = 10.0
+LR_BASE = 0.01
 LR_DECAY_FACTOR = 1
-BATCH_SIZE = 128
+BATCH_SIZE = 32
+
+class ModelMGPU(keras.models.Model):
+    def __init__(self, ser_model, gpus):
+        pmodel = multi_gpu_model(ser_model, gpus)
+        self.__dict__.update(pmodel.__dict__)
+        self._smodel = ser_model
+
+    def __getattribute__(self, attrname):
+        '''Override load and save methods to be used from the serial-model. The
+           serial-model holds references to the weights in the multi-gpu model.
+           '''
+        if 'load' in attrname or 'save' in attrname:
+           return getattr(self._smodel, attrname)
+        else:
+           #return Model.__getattribute__(self, attrname)
+           return super(ModelMGPU, self).__getattribute__(attrname)
 
 if __name__ == '__main__':
     config = tf.ConfigProto()
@@ -31,9 +51,9 @@ if __name__ == '__main__':
     session = tf.Session(config=config)
     K.set_session(session)
     # input generators
-    train_datagen = ImageDataGenerator(rotation_range=5, width_shift_range=0.2,
-                                       height_shift_range=0.2, brightness_range=(0.85, 1.15),
-                                       shear_range=0.0, zoom_range=0.3,
+    train_datagen = ImageDataGenerator(rotation_range=5, width_shift_range=0.1,
+                                       height_shift_range=0.1, brightness_range=(0.9, 1.1),
+                                       shear_range=0.0, zoom_range=0.2,
                                        channel_shift_range=0.2,
                                        fill_mode='reflect', horizontal_flip=True,
                                        vertical_flip=False, rescale=1/255,
@@ -60,9 +80,9 @@ if __name__ == '__main__':
     x = base_model.output
     predictions = Dense(N_CLASSES, activation='softmax')(x)
     model = keras.models.Model(inputs=base_model.input, outputs=predictions)
+    model = ModelMGPU(model, 2)
     decay = LR_BASE/(EPOCHS * LR_DECAY_FACTOR)
-    sgd = keras.optimizers.SGD(
-        lr=LR_BASE, decay=decay, momentum=0.9, nesterov=True)
+    sgd = keras.optimizers.SGD(lr=LR_BASE, decay=decay, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
@@ -75,7 +95,7 @@ if __name__ == '__main__':
     ckpt = keras.callbacks.ModelCheckpoint(os.path.join(checkpoint_path, 'model.{epoch:02d}-{val_acc:.2f}.h5'),
                                            monitor='val_acc', verbose=1, save_best_only=True)
     reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10,
-                                                  verbose=1, mode='auto', min_delta=0.001,
+                                                  verbose=1, mode='auto',# min_delta=0.001,
                                                   cooldown=0, min_lr=0)
     log_dir = "logs_mobile/model_{}_{}".format(
         MODEL_NO, datetime.utcnow().strftime("%d%m%Y_%H%M%S"))
