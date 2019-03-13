c40d20ada186d7cf1b0ecd47c4096fca11dcec3b
Sun Yitao <sunyitao8@gmail.com>
2019-03-13 14:03:09 +0800

    asda

--- title_classification/fashion_magpie.py
+++ title_classification/fashion_magpie.py
@@ -13,7 +13,7 @@ keras.backend.set_session(session)
 data_dir = Path.cwd().parent / 'data'
 labels = [str(x) for x in range(17,31)]
 ckpt_dir = data_dir / 'magpie' / 'checkpoints' / 'fashion' / 'v1'
-os.makedirs(str(ckpt_dir), exist_ok=True)
+os.makedirs(str(ckpt_dir / 'scaler.pkl'), exist_ok=True)
 
 ckpt = keras.callbacks.ModelCheckpoint(os.path.join(ckpt_dir, 'model.{epoch:02d}-{val_acc:.2f}.h5'),
                                        monitor='val_acc', verbose=1, save_best_only=True)
@@ -29,6 +29,6 @@ magpie.batch_train(str(data_dir / 'magpie' / 'fashion' / 'train'), labels,
                    batch_size=256, epochs=30, callbacks=[ckpt, reduce_lr], verbose=2)
 
 
-magpie.save_scaler(str(ckpt_dir), overwrite=True)
+#magpie.save_scaler(str(ckpt_dir / 'scaler.pkl'), overwrite=True)
 magpie.save_model(str(ckpt_dir / 'magpie.h5'))
-magpie.save_word2vec_model(str(ckpt_dir))
+magpie.save_word2vec_model(str(ckpt_dir / 'word2vec.pkl'))
