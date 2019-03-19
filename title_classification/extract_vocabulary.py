a7b066ae0a5eb1c71981593622f8e95be18e09e6
LoveNordling <love_nordling@hotmail.com>
2019-03-19 23:41:20 +0800

    Major bug. Forgot tosplit titles.

--- title_classification/extract_vocabulary.py
+++ title_classification/extract_vocabulary.py
@@ -16,9 +16,9 @@ def split_categories(train):
     for i in range(1,58):
         row_of_is = train.loc[train['Category'] == i]
         title, category = row_of_is['title'], row_of_is['Category']
-        category_split.append(title)
-
-    return category_split
+        title_words = [word for line in title for word in line.split()] #split into words
+        category_split.append(title_words)
+    return pd.DataFrame(category_split)
 
 #Return titles that repeat
 def get_non_unique_titles(all_titles):
@@ -44,7 +44,7 @@ def get_word_counters(category_split):
 
 
 category_split = split_categories(train)
-
+print(category_split)
 
 #Within each class find unique titles and then add them into one list
 title_all_classes = []
@@ -54,7 +54,7 @@ for titles in category_split:
     for s in train_set:
         word_counter.update(s.split())
 
-    title_all_classes+=list(word_counter)
+    title_all_classes = title_all_classes + list(word_counter)
 
 
 #find non unique titles
@@ -63,23 +63,27 @@ not_unique_titles = get_non_unique_titles(title_all_classes)
 
 print(not_unique_titles)
 print("Number of non unique titles " + str(len(not_unique_titles)))
-print("Number of titles " + str(len(train.title)))
 
+all_titles = train["title"]
+title_words = pd.DataFrame([word for line in all_titles for word in line.split()] )
+print("Number of titles " + str(len(title_words)))
 # delete non unique titles
-train = train[train.title.isin(not_unique_titles)]
+unique_category_split = []
+for titles in category_split:
+    unique_category_split.append(titles[titles[0].isin(not_unique_titles)])
 
 print("Done with cleaning")
-print("Number of titles" + str(len(train.title)))
+print("Number of titles " + str(sum(map(len, unique_category_split))))
 
 print("All unique titles")
-print(train.title)
+print(unique_category_split)
 
 #Get word counts for unique titles
 category_split = split_categories(train)
 word_counters = get_word_counters(category_split)
 
 
-for counter in word_counters:
-    print(counter)
+#for counter in word_counters:
+    #print(counter)
 
 
