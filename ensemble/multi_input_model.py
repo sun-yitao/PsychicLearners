a4154914f9209da986d4eebfb073e45dc57d6b87
Sun Yitao <sunyitao8@gmail.com>
2019-03-19 22:28:18 +0800

    multi input model

--- ensemble/multi_input_model.py
+++ ensemble/multi_input_model.py
@@ -107,7 +107,7 @@ def train_model(train_gen, valid_gen, class_weights=None):
     image_model.layers.pop()  # remove fully connected layers
     image_model.layers.pop()  # remove fully connected layers
     image_model.layers.pop()  # remove pooling
-    image_model = keras.models.Model(inputs=image_model.input, outputs=image_model.layers.output)
+    image_model = keras.models.Model(inputs=image_model.input, outputs=image_model.output)
     print(image_model.summary())
     multi_inp_model = multi_input_model(image_model, vocab_size=vocab_size, k_reg=0)
     sgd = keras.optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
